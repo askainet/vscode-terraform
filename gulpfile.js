54bff9afdb5a9edf7f568ce6a97e162fe8d2bf52
Mikael Olenfalk <mikael@olenfalk.se>
2018-07-10 22:32:49 +0200

    Use gulp-hub instead of require

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var path = require('path');
 var log = require('fancy-log');
 var chalk = require('chalk');
 var mocha = require('gulp-mocha');
+var hub = require('gulp-hub');
 
 var spawn = require('child_process').spawn;
 var fs = require('fs');
@@ -15,8 +16,8 @@ var fs = require('fs');
 var helpers = require('./tasks/helpers');
 
 // load tasks
-require("./tasks/auto-completion-data.task.js");
-require("./tasks/provider-documentation.task.js");
+var registry = new hub(['./tasks/*.task.js']);
+gulp.registry(registry);
 
 // generate hcl wrapper
 gulp.task('generate-hcl-container', (done) => {
